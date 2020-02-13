34abc2c78fbc86c4164bd54756f34616ae95167e
Omie Walls <omierwalls@gmail.com>
2020-02-13 18:23:37 -0500

    Clearing notifications on command

--- src/app/pages/lobby/lobby.component.ts
+++ src/app/pages/lobby/lobby.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/firebase/auth/auth.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {ProfileService} from '../../services/profile/profile.service';
 import {IProfile} from '../../classes/profile';
 import {NotificationService} from '../../services/notification/notification.service';
 import {INotification, NotificationStatus, NotificationType} from '../../classes/notification';
 import {IconService} from '../../services/util/icon/icon.service';
+import {NotificationProfileService} from '../../services/profile/notification-profile.service';
 
 @Component({
   selector: 'app-lobby',
@@ -19,7 +19,7 @@ export class LobbyComponent implements OnInit {
   notificationTypes = NotificationType;
   notificationStatuses = NotificationStatus;
   constructor(private auth: AuthService,
-              private profileService: ProfileService,
+              private profileService: NotificationProfileService,
               private router: Router,
               private route: ActivatedRoute,
               private notificationService: NotificationService,
@@ -45,5 +45,9 @@ export class LobbyComponent implements OnInit {
   async markNotificationsAsRead() {
     await this.notificationService.markAllAsRead();
     this.all = await this.notificationService.all();
+  }
+
+  async clearNotifications() {
+    await this.profileService.clearNotifications();
   }
 }
